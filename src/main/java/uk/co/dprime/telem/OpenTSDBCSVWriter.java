84e2406d322140880d360fc9abfcab28da1ba88a
dprime <david@primefarm.co.uk>
2015-06-17 21:33:44 +0100

    pass the host/port in as command line args tidy up some comments and whitespace

--- src/main/java/uk/co/dprime/telem/OpenTSDBCSVWriter.java
+++ src/main/java/uk/co/dprime/telem/OpenTSDBCSVWriter.java
@@ -12,12 +12,24 @@ public class OpenTSDBCSVWriter {
 
     private final String inputfile;
 
-    public OpenTSDBCSVWriter(String inputfile) {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String inputfile = args[0];
+        String host = args[1];
+        int port = Integer.parseInt(args[2]);
+
+        OpenTSDBCSVWriter spammer = new OpenTSDBCSVWriter(inputfile, host, port);
+        while(true) {
+            long now = System.currentTimeMillis();
+            spammer.start(now);
+        }
+    }
+
+    public OpenTSDBCSVWriter(String inputfile, String host, int port) {
         this.inputfile = inputfile;
         //because the writer is static, set the host/port values here so that when the threadlocal kicks
         //in it connects to the right place.
-        TSDBWriter.host = "";
-        TSDBWriter.port = 4242;
+        TSDBWriter.host = host;
+        TSDBWriter.port = port;
     }
 
 
@@ -28,18 +40,13 @@ public class OpenTSDBCSVWriter {
         Map<String, Object> tags = new HashMap<>();
         tags.put("tag", "value");
 
-        long lastTime = 0;
-
         while ((line = br.readLine()) != null) {
-
-            // use comma as separator
             String[] split = line.split(",");
 
             if(headers == null) {
                 headers = split;
                 continue;
             }
-
 
             long now = System.currentTimeMillis();
 
@@ -53,8 +60,6 @@ public class OpenTSDBCSVWriter {
             //what's the difference between now-startTime and millisSinceStart
             long millisSinceBase = now - baseTime;
             long diff = millisSinceStart - millisSinceBase;
-
-
 
             //otherwise, do the actual sending
             for (int i = 1; i < split.length; i++) {
@@ -73,26 +78,6 @@ public class OpenTSDBCSVWriter {
             if(diff > 0) {
                 Thread.sleep(diff);
             }
-
-
         }
-    }
-
-
-
-
-
-
-    public static void main(String[] args) throws IOException, InterruptedException {
-        String inputfile = args[0];
-
-        OpenTSDBCSVWriter spammer = new OpenTSDBCSVWriter(inputfile);
-        while(true) {
-            long now = System.currentTimeMillis();
-            spammer.start(now);
-
-            int lol = 5;
-        }
-
     }
 }
